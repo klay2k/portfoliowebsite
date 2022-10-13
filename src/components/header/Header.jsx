4abd9721955a1b6bfeb65b84029edc828b76913b
Clayton Hiebert <clayton.hiebert1@gmail.com>
2022-10-12 20:26:56 -0500

    styling profile photo with background

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/My project.png'
+import ME from '../../assets/My project (2).png'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
@@ -18,7 +18,7 @@ const Header = () => {
             <img src={ME} alt="me" />
             </div>
 
-            <a href="#contact" className='scroll_down'>Scroll Down</a>
+            <a href="#contact" className='scroll__down'>Scroll Down</a>
         </div>
     </header>
   )

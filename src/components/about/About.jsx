e113145dd820e6d6250759ae8d6c4b429a605415
Clayton Hiebert <clayton.hiebert1@gmail.com>
2022-10-15 11:49:43 -0500

    about component styling

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/IMG_0391.JPG'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1 Year of Coding</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>Want to be my first?</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>3 Completed</small>
+            </article>
+          </div>
+          
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique modi assumenda quod ratione, doloremque incidunt aliquam enim aspernatur ea dolorem at voluptates quae possimus odio ut nihil voluptatibus corporis ad.
+            </p>
+
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 

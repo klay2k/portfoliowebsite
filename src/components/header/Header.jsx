edb513fa0f7af219807f9372a2491bc82a83fce3
Jeffrey Piccirillo <piccirilloj1@gmail.com>
2022-10-21 18:09:07 -0500

    Implement ScrollDown feature which rotates through sections of page
    - ScrollDown feature is aware of what the sections are via config in sections.js
    - Current section is available to us at window.location.hash ("Window" is one of the main Browser APIs).  We look for this current section in the sections.js config, then advance to the next one
    - Once the next section's anchor is identified, then set the site's window.location.hash to it
    - the scroll_down DOM element can no longer be an anchor bc its href tag doesnt really mesh well with the necessary onClick prop.  It's not really an anchor anymore either, it's just a button in disguise.  Also update its style to actually be fixed to side of browser window during scroll (this is position: fixed)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,23 +3,37 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/My project (2).png'
 import HeaderSocial from './HeaderSocials'
+import { sections } from '../../sections'
 
 const Header = () => {
+  function getNextSection() {
+    // Get the index of the next section, if it's out of bounds then wrap to beginning
+    const currentSection = window.location.hash || "#";
+    const nextSectionIndex =
+      sections.findIndex((s) => s.anchor === currentSection) + 1;
+    const wrappedValue = nextSectionIndex % sections.length;
+
+    // Set page location to the new anchor
+    window.location.hash = sections[wrappedValue].anchor;
+  }
+
   return (
     <header>
-        <div className="container header__container">
-            <h5>Hello I'm</h5>
-            <h1>Clayton Hiebert</h1>
-            <h5 className="text-light">Fullstack Developer</h5>
-            <CTA />
-            <HeaderSocial />
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Clayton Hiebert</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocial />
 
-            <div className="me">
-            <img src={ME} alt="me" />
-            </div>
-
-            <a href="#contact" className='scroll__down'>Scroll Down</a>
+        <div className="me">
+          <img src={ME} alt="me" />
         </div>
+
+        <div onClick={getNextSection} className="scroll__down">
+          Next Section
+        </div>
+      </div>
     </header>
   )
 }
